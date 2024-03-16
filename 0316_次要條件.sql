fba1a7a933d7b6683890ffe66fe77e0530ba2740
Alice <tefanie0603@gmail.com>
2024-03-16 10:24:08 +0800

    Update 0316_次要條件.sql

--- 0316_次要條件.sql
+++ 0316_次要條件.sql
@@ -1,6 +1,6 @@
-/*進站人數最多的一筆*/  /*要注意若兩邊命名一樣前面要加.*/
+/*進站人數最多的一筆*/ 
 SELECT * 
-FROM gate_count LEFT JOIN stations ON 站點編號=編號
+FROM gate_count LEFT JOIN stations ON 站點編號=編號     /*要注意若兩邊命名一樣前面要加.*/
 WHERE 進站人數 =(SELECT MAX(進站人數) FROM gate_count);
 
 /*SQL無法分段式紀錄先求A值再求B值所以必須一氣呵成寫完一個公式*/ 
@@ -13,3 +13,21 @@ WHERE 進站人數 = MAX(進站人數) /*這是錯的*/
 
 SELECT MAX(進站人數) /*這是對的*/
 FROM gate_count
+
+
+/*各站點進站人數最多的一筆*/ 
+
+/*第一種寫法*/ 
+SELECT 站點編號,進站人數
+FROM gate_count LEFT JOIN stations ON 站點編號=編號
+WHERE (站點編號,進站人數) IN(
+	SELECT 站點編號 ,MAX(進站人數)
+    FROM gate_count
+    GROUP BY 站點編號);
+
+/*GROUP BY 站點編號,進站人數*/ 
+
+/*第一種寫法*/ 
+SELECT 站點編號 ,MAX(進站人數)
+FROM gate_count
+GROUP BY 站點編號
